7b63fca06b009b2e12556d909d2d3a1619f15629
johnnyghost <kemarx@gmail.com>
2018-09-10 16:40:39 +0100

    feat: Split routes.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import express from 'express';
+import app from './config/express';
 
-const app: express.Application = express();
 const port: any = process.env.PORT || 3000;
-
-app.use('/welcome', (req, res) => res.send('Hello Worlds!'));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
