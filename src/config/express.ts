f635bafae268cdbc7a0e31427f2e99c78fa19703
johnnyghost <kemarx@gmail.com>
2018-09-12 15:36:13 +0100

    refactor.

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,8 +1,42 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './../modules';
 
-const app: express.Application = express();
+class App {
 
-app.use('/', routes);
+  public app: express.Application;
 
-export default app;
+  /**
+   * Initiate and configure the App.
+   *
+   * @constructor
+   */
+  constructor() {
+    this.app = express();
+
+    this.routes();
+    this.config();
+  }
+
+  /**
+   * Initiate the routes.
+   */
+  private routes():void {
+    this.app.use('/', routes);
+  }
+
+  /**
+   * Config the app
+   */
+  private config():void {
+    // support application/json type post data
+    this.app.use(bodyParser.json());
+    // support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // enable CORS - Cross Origin Resource Sharing
+    this.app.use(cors());
+  }
+}
+
+export default new App().app;
